4852057ec4085331275b58a763ee09084ab0758c
Noah Saso <noahsaso@berkeley.edu>
2020-07-29 23:16:37 -0700

    Update odbcss.h with new number types

--- SQLClient/SQLClient/SQLClient/odbcss.h
+++ SQLClient/SQLClient/SQLClient/odbcss.h
@@ -129,22 +129,22 @@ extern "C" {
 #endif
 
 typedef struct tagSS_TIME2_STRUCT {
-	SQLUSMALLINT hour;
-	SQLUSMALLINT minute;
-	SQLUSMALLINT second;
-	SQLUINTEGER fraction;
+	CS_USMALLINT hour;
+	CS_USMALLINT minute;
+	CS_USMALLINT second;
+	CS_UINT fraction;
 } SQL_SS_TIME2_STRUCT;
 
 typedef struct tagSS_TIMESTAMPOFFSET_STRUCT {
-	SQLSMALLINT year;
-	SQLUSMALLINT month;
-	SQLUSMALLINT day;
-	SQLUSMALLINT hour;
-	SQLUSMALLINT minute;
-	SQLUSMALLINT second;
-	SQLUINTEGER fraction;
-	SQLSMALLINT timezone_hour;
-	SQLSMALLINT timezone_minute;
+	CS_SMALLINT year;
+	CS_USMALLINT month;
+	CS_USMALLINT day;
+	CS_USMALLINT hour;
+	CS_USMALLINT minute;
+	CS_USMALLINT second;
+	CS_INT fraction;
+	CS_SMALLINT timezone_hour;
+	CS_SMALLINT timezone_minute;
 } SQL_SS_TIMESTAMPOFFSET_STRUCT;
 
 
